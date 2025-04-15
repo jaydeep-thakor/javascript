d427c759da1a5aba84c14dabb4ae2418f46462e1
Jaydeep <75525428+jaydeep-thakor@users.noreply.github.com>
2025-04-15 07:34:16 +0530

    Delete 87 directory

--- 87/path.js
+++
@@ -1,9 +0,0 @@
-import path from "path";
-
-let myPath = "E:\\javascript\\87\\about.txt";
-
-console.log(path.extname(myPath))
-console.log(path.dirname(myPath))
-console.log(path.basename(myPath))
-
-console.log(path.join("E:/", "programs\\about.txt"))
